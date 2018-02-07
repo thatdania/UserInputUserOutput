eb3fa620bf4e7e0a0ade9b34942a9348d9482e68
thatdania@gmail.com <thatdania@gmail.com>
2018-02-07 14:05:42 +0000

    Create two components in React call user input and output base on instructions

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const userInput = () => {
+  return (
+  <div className="userInput">
+    <input type="text" value="value"/>
+
+  </div>
+  )
+}
+
+
+export default userInput;
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const userOutput = () => {
+  return (
+  <p> Hello </p>
+  <p> Bello </p>
+  )
+}
+
+
+export default userOutput;
